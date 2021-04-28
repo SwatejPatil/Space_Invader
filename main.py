effc745693c845ef082985139ae0ccc0c92a644f
SwatejPatil <81546709+SwatejPatil@users.noreply.github.com>
2021-04-28 17:10:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ class Enemy(Ship):
 
 	def shoot(self):
 		if self.cool_down_counter == 0:
-			laser = Laser(self.x-20, self.y, self.laser_img)
+			laser = Laser(self.x+10, self.y, self.laser_img)
 			self.lasers.append(laser)
 			self.cool_down_counter = 1
 
